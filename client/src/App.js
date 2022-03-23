49afb0f06136ebd62e594041d94f21f0e39a4623
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-03-23 11:46:14 -0400

    route

--- client/src/App.js
+++ client/src/App.js
@@ -25,19 +25,19 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
+      <Router>
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/thought" component={SingleThought} />
+      </Router>
           </div>
           <Footer />
         </div>
-      </Router>
     </ApolloProvider>
   );
 }
